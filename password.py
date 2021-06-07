35b68d2ad212787cfe376089051fed6cdd286d8c
days109 <larstraschen@gmail.com>
2021-06-07 23:47:18 +0800

    teacher ver.

--- password.py
+++ password.py
@@ -1,13 +1,14 @@
 x = 3
-while True:
+while x > 0:
 	x = x - 1
 	password = input('請輸入密碼：')
-
-	if x == 0:
-		print('密碼錯誤已達上限')
-		break
+		
 	if password == 'a123456':
 		print('登入成功')
 		break
 	else:
-		print('密碼錯誤！還有', x, '機會')
+		if x > 0:
+			print('密碼錯誤！還有', x, '機會')
+		else:
+			print('密碼錯誤已達上限')
+
